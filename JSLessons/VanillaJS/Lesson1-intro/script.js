97d6b543d84cc669294974d1dfdd3f7e22448f53
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-04-13 12:00:13 +0400

    js lesson start

---
+++ JSLessons/VanillaJS/Lesson1-intro/script.js
@@ -0,0 +1,22 @@
+/**
+ * more 
+ * line 
+ * comment
+ */
+
+//one line
+
+// debugger;
+// console.log("Hello1");
+// console.log("Hello2");
+// console.log("Hello3");
+// console.log("Hello4");
+// console.log("Hello5");
+// console.log("Hello6");
+// console.log("Hello7");
+
+// alert("Hello");
+// window.alert("Hello1");
+
+// const a = prompt();
+// document.write(a);
---
+++ JSLessons/VanillaJS/Lesson2-dataTypes/js/main.js
@@ -0,0 +1,33 @@
+// Data Types (Data tipləri)
+
+// Primitive DT -> Bəsit DT
+// String
+var a = "Hello";
+var b = " JavaScript";
+console.log(a + b);
+
+// Number
+console.log("510" + 5);
+
+// Boolean
+var e = true;
+var x = false;
+console.log(x);
+// =========================================================
+
+// Composite DT -> Birləşik DT
+// Array
+var myData = ["table", 4, true];
+console.log(myData);
+// Object
+var myObj = { name: "Kamran", age: 20 };
+console.log(myObj);
+// =========================================================
+
+// Special   DT -> Xüsusi DT
+// undefined
+var infoUn = undefined;
+console.log(infoUn);
+// null
+var infoNull = null;
+console.log(infoNull);

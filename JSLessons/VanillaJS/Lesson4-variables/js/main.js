d404a42ba11a9c856de946b70452ceeb98e8becf
HAsan Bakhtiar <hasan.doctype@gmail.com>
2022-04-20 12:11:35 +0400

    loops

---
+++ JSLessons/VanillaJS/Lesson4-variables/js/main.js
@@ -0,0 +1,90 @@
+// var info = "Hello";
+// console.log(info);
+
+// 1.Dəyişkən adları mənalı və anlaşıqlı olmalıdır.
+// 2.Dəyişkən adlarını yazarkən İngilis əlifbasından başqa əlifadan istifadə etmək qadağandır.
+// 3.Dəyişkən adları _ (alt xətt) ilə başlaya bilər.
+// 4.Dəyişkən adları $(dollar) işarəsi ilə başlaya bilər.
+// 5.Dəyişkən adları heç bir zaman rəqəm ilə başlaya bilməz.Amma ilk hərfdə sonra istifadə edə bilərik.
+// 6.Dəyişkən adları böyük kiçik hərfləri anlayır.
+// 7.Eyni dəyişkənə iki fərqli məlumat tanımlanan zaman sonuncu tanımlanan məlumatı göstərir.
+// 8.JavaScript tərəfindən hazırlanmış rezerv kəlimələri dəyişkən adlarına vermək qadağandır.
+// 9.Eyni dəyişkən adını dəişkən ifadələrində istifadə etmək meslehetli deyil.
+
+// abstract 	    arguments	    await*	            boolean
+// break	        byte	        case	            catch
+// char	            class*	        const	            continue
+// debugger	        default	        delete	            do
+// double	        else	        enum*	            eval
+// export*	        extends*	    false	            final
+// finally	        float	        for	                function
+// goto	            if	            implements	        import*
+// in	            instanceof	    int	                interface
+// let*	            long	        native	            new
+// null	            package	        private	            protected
+// public	        return	        short	            static
+// super*	        switch	        synchronized	    this
+// throw	        throws	        transient	        true
+// try	            typeof	        var	                void
+// volatile	        while	        with	            yield
+
+// var          variable    global
+// const        constant    local
+// let          let         local
+
+// const a = "Hello";
+//  a = "Hello1";
+// console.log(a);
+
+// global
+// if (true) {
+//   const b = "Hello";
+//   // local global
+//   console.log(b);
+//   if (true) {
+//     // local global
+//     const b = "new b";
+//     console.log(b);
+//     if (true) {
+//       // local
+//     }
+//   }
+// }
+
+// const info = 564546;
+
+// switch (info) {
+//   case 5:
+//     console.log("info = 5");
+//     break;
+//   case "hello":
+//     console.log("Hello = info");
+//     break;
+//   default:
+//     console.log("duzgun deyer ver");
+// }
+
+// const myTime = 5;
+
+// switch (myTime) {
+//   case 5:
+//     if (myTime <10) {
+//         console.log("a");
+//     }
+//     break;
+// default:
+//     console.log('error');
+// }
+
+
+// try {
+//         throw " her sey eladi";
+//     consoale.log('a');
+// } catch (err) {
+//     console.log(err);
+//     // console.log("burda bir error var");
+// }finally{
+//     console.log("melumat yuklenir");
+// }
+
+
---
+++ JSLessons/VanillaJS/lesson5-loops/js/main.js
@@ -0,0 +1,25 @@
+//Loops -> donguler
+// let info = 10;
+
+// while(info>=0){
+//     console.log(info+" Hello");
+//     info--;
+
+// }
+
+// do{
+//     console.log(info);
+//     info++;
+// }while(info>=10);
+
+// for (let a = 0; a < 10; a++) {
+//   console.log(a+" Hello");
+// }
+
+
+// let myData = ['table','book','pen']
+// console.log(myData);
+// for (let index = 0; index < myData.length; index++) {
+//     console.log(myData[index]);
+    
+// }

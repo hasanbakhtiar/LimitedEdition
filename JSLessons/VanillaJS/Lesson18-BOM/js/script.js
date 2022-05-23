65791cd0ed09dfff1f5fe8c5369da3eb6b107068
HAsan Bakhtiar <hasan.doctype@gmail.com>
2022-05-23 16:48:44 +0400

    BOM

---
+++ JSLessons/VanillaJS/Lesson18-BOM/js/script.js
@@ -0,0 +1,39 @@
+const info = document.createElement('h1');
+// info.innerHTML = `${window.innerWidth}px`;
+// info.innerHTML = `${window.innerHeight}px`;
+// info.innerHTML = `${screen.width}px`;
+// info.innerHTML = `${screen.height}px`;
+// info.innerHTML = `${screen.availHeight}px`;
+// info.innerHTML = `${screen.availWidth}px`;
+// info.innerHTML = `ColorDepth:${screen.colorDepth}`;
+// info.innerHTML = `PixelDepth:${screen.pixelDepth}`;
+// info.innerHTML = `Location:${window.location.href}`;
+// info.innerHTML = `Location:${window.location.host}`;
+// info.innerHTML = `Location:${window.location.hostname}`;
+// info.innerHTML = `Location:${window.location.pathname}`;
+// info.innerHTML = `Location:${window.location.protocol}`;
+
+const body = document.querySelector('body');
+body.appendChild(info);
+
+let box;
+document.querySelector('#i').onclick=()=>{
+    // window.open('home.html',"Hello","width=700,height=500");
+//    box = window.open('home.html',"Hello","width=700,height=500");
+//    document.querySelector("h1").classList.toggle('a');
+//    document.querySelector("h1").classList.add('a');
+  
+}
+
+document.querySelector('#c').onclick=()=>{
+    // window.close()
+    // box.moveTo(500, 400);
+    // box.resizeTo(100,100);
+//    document.querySelector("h1").classList.remove('a');
+// history.back();
+// history.forward();
+
+}
+
+
+

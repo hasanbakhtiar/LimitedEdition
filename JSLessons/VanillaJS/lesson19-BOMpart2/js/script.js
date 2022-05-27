9fa8c485e94ba08c8bedd2dbb10cf295892de733
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-05-27 17:11:12 +0400

    methods

---
+++ JSLessons/VanillaJS/lesson19-BOMpart2/js/script.js
@@ -0,0 +1,13 @@
+const text = document.querySelector('p');
+
+// text.innerText = "<h1>Hello</h1>";
+// text.innerHTML = "<h1>Hello</h1>";
+// text.innerHTML = navigator.product;
+// text.innerHTML = navigator.appCodeName;
+// text.innerHTML = navigator.appName;
+// text.innerHTML = navigator.userAgent ;
+// text.innerHTML = navigator.language ;
+// text.innerHTML = navigator.onLine ;
+
+const a = window.prompt('Enter ur name');
+text.innerHTML = a; 
---
+++ JSLessons/VanillaJS/lesson20-methods/js/script.js
@@ -0,0 +1,37 @@
+// https://www.w3schools.com/jsref/jsref_obj_regexp.asp
+// let a = "Javascript";
+// console.log(a.match(/a/ig));
+
+// const a = "Hello, How a u";
+// const b = /[^e]/g;
+// console.log(a.match(b));
+
+// let text = "";
+// const fruits = ["apple", "orange", "cherry",'banana'];
+// fruits.forEach(myFunction);
+
+// document.getElementById("demo").innerHTML = text;
+ 
+// function myFunction(eloset, say) {
+//   text += eloset + say +"<br>"
+// }
+
+// let infoArr  = ["hello", 'mello' ,'tello'];
+// // infoArr.push('cello');
+// // infoArr.pop();
+// // infoArr.shift();
+// // infoArr.unshift('cello');
+// console.log(infoArr);
+
+
+// class Car{
+//     constructor(name){
+//         this.name = name;
+//     }
+// }
+// Car.prototype.age = "19"
+// const a = new Car("Hello");
+// console.log(a.name);
+// console.log(a.age);
+
+

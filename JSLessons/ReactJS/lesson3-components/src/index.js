d4ecaa4a8d76babb4e72482caaedb9aa16d08cf4
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-06-27 17:05:48 +0400

    react-part1

---
+++ JSLessons/ReactJS/lesson3-components/src/index.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import './sass/style.css';
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
+
+
+class Card extends Component{
+  render(){
+    return(
+      <div className="card" style={{ width: '18rem' }}>
+      <img src="..." className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+    )
+  }
+}
+
+class SecOne extends Component {
+  render() {
+    return (
+      <div className="row">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      </div>
+    )
+  }
+}
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Nav />
+        <SecOne />
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<App />, document.querySelector("#root"));
